7edf53a1aa29025143e781509918c78956b28369
yd_private <690462878@qq.com>
2019-11-14 21:37:39 +0800

    temp

--- StationLog/ReportTimeWindow.xaml.cs
+++ StationLog/ReportTimeWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 
 namespace StationLog
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// ReportTimeWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class ReportTimeWindow : Window
     {
-        public MainWindow()
+        public ReportTimeWindow()
         {
             InitializeComponent();
-        }
-
-        private void Login(object sender, RoutedEventArgs e)
-        {
-            LoginWindow window = new LoginWindow();
-            window.ShowDialog();
         }
     }
 }
