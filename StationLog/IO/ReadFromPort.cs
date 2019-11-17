9a47680a9751d852555a70d2e7f20a636f9835a7
yd_private <690462878@qq.com>
2019-11-17 22:55:49 +0800

    temp

--- StationLog/IO/ReadFromPort.cs
+++ StationLog/IO/ReadFromPort.cs
@@ -12,33 +12,21 @@ namespace StationLog.IO
     {
         public static void ReceiveMsg()
         {
-            //while (true) {
-            //    var factory = new ConnectionFactory() { HostName = "39.108.177.237" };
-            //    using (var connection = factory.CreateConnection())
-            //    using (var channel = connection.CreateModel())
-            //    {
-            //        channel.ExchangeDeclare(exchange: "", type: "topic");
-            //        var queueName = channel.QueueDeclare().QueueName;
-
-            //        channel.QueueBind(queue: queueName,
-            //                          exchange: "",
-            //                          routingKey: "anonymous.info");
-
-            //        var consumer = new EventingBasicConsumer(channel);
-            //        consumer.Received += (model, ea) =>
-            //        {
-            //            var body = ea.Body;
-            //            var message = Encoding.UTF8.GetString(body);
-            //            var routingKey = ea.RoutingKey;
-            //            Console.WriteLine("Received '{0}':'{1}'",
-            //                              routingKey,
-            //                              message);
-            //        };
-            //        //channel.BasicConsume(queue: queueName,
-            //        //                     autoAck: true,
-            //        //                     consumer: consumer);
-            //    }
-            //}
+            ConnectionFactory factory = new ConnectionFactory { HostName = "39.108.177.237", UserName = "admin", Password = "admin", VirtualHost = "/" };
+            using (IConnection conn = factory.CreateConnection())
+            {
+                using (IModel im = conn.CreateModel())
+                {
+                    while (true)
+                    {
+                        BasicGetResult res = im.BasicGet("rabbitmq_query", true);
+                        if (res != null)
+                        {
+                            Console.WriteLine("receiver:" + Encoding.UTF8.GetString(res.Body));
+                        }
+                    }
+                }
+            }
         }
     }
 }
