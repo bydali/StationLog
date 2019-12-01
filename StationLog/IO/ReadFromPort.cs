feba719ae7575392f7a80cc0427eb882f6f733af
yd_private <690462878@qq.com>
2019-12-01 18:20:18 +0800

    temp

--- StationLog/IO/ReadFromPort.cs
+++ StationLog/IO/ReadFromPort.cs
@@ -12,25 +12,7 @@ namespace StationLog.IO
     {
         public static void ReceiveMsg()
         {
-            ConnectionFactory factory = new ConnectionFactory { HostName = "39.108.177.237", UserName = "admin", Password = "admin" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    var consumer = new QueueingBasicConsumer(channel);
-                    channel.BasicConsume("Q2", true, consumer);
 
-                    while (true)
-                    {
-                        var ea = consumer.Queue.Dequeue();
-
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body);
-
-                        Console.WriteLine("Received {0}", message);
-                    }
-                }
-            }
         }
     }
 }
--- StationLog/IO/WriteToPort.cs
+++ StationLog/IO/WriteToPort.cs
@@ -11,23 +11,7 @@ namespace StationLog.IO
     {
         public static void SendMsg()
         {
-            ConnectionFactory factory = new ConnectionFactory { HostName = "39.108.177.237", UserName = "admin", Password = "admin" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.ExchangeDeclare("amq.topic", "topic", true, false, null);
 
-                    var routingKey = "q1";
-                    var message = "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
-                    channel.BasicPublish(exchange: "amq.topic",
-                                         routingKey: routingKey,
-                                         basicProperties: null,
-                                         body: body);
-                    Console.WriteLine("Sent {0}", message);
-                }
-            }
 
         }
 
